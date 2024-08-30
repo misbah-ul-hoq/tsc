dc889c00bf8feec5c78bb488ce4783e93477fd36
Misbah Ul Hoq <misbahul@techjoule.com>
2024-08-30 22:43:54 +0600

    tutors name displayed

--- src/components/Shared/Tutors.tsx
+++ src/components/Shared/Tutors.tsx
@@ -4,10 +4,11 @@ import api from "../../axios/api";
 type Tutor = {
   _id: string;
   email: string;
-  name: string;
+  displayName: string;
   photoURL: string;
   role: string;
 };
+
 const Tutors = () => {
   const [tutors, setTutors] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -32,20 +33,21 @@ const Tutors = () => {
       {!loading && (
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {tutors.map((tutor: Tutor) => {
-            const { _id, role, photoURL, name, email } = tutor;
+            const { _id, role, photoURL, displayName, email } = tutor;
             return (
               <div key={_id} className="">
                 <div className="card bg-base-100 shadow-xl">
                   <figure className="px-10 pt-10">
                     <img
                       src={photoURL}
-                      alt={`${name}'s Photo`}
+                      alt={`${displayName}'s Photo`}
                       className="rounded-full w-32 h-32 object-cover"
                     />
                   </figure>
                   <div className="card-body pt-4 items-center text-center">
-                    <h2 className="card-title">{name}</h2>
+                    <h2 className="card-title">{displayName}</h2>
                     <p className="text-gray-500">{email}</p>
+                    <p className="text-gray-500">{displayName}</p>
                     <div className="card-actions">
                       <div className="badge badge-neutral">{role}</div>
                     </div>
