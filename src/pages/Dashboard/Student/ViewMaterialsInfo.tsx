5e990d99e276ec98cadc4c03fd5184333148784e
Misbah Ul Hoq <misbahul@techjoule.com>
2024-09-01 10:45:35 +0600

    updated useParams

--- src/pages/Dashboard/Student/ViewMaterialsInfo.tsx
+++ src/pages/Dashboard/Student/ViewMaterialsInfo.tsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const ViewMaterialsInfo = () => {
+  const params = useParams();
+  console.log(params);
   return <div>ViewMaterialsInfo</div>;
 };
 
