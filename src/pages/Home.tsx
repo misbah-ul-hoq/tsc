fd8c96b24cd804486c8a3f93626c90deb2e24889
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-10-05 22:27:13 +0600

    installed react awesome reveal and used that on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,22 @@ import useDocumentTitle from "dynamic-title-react";
 import Banner from "../components/Home/Banner";
 import StudySessions from "../components/Home/StudySessions";
 import Tutors from "../components/Shared/Tutors";
+import { Fade, Slide, Zoom } from "react-awesome-reveal";
 const Home = () => {
   useDocumentTitle("Teacher Student Centre");
   return (
     <main className="my-4 py-4 space-y-10 text-lg">
-      <Banner />
-      <StudySessions />
-      <Tutors />
+      <Slide>
+        <Banner />
+      </Slide>
+
+      <Fade fraction={0.25}>
+        <StudySessions />
+      </Fade>
+
+      <Zoom>
+        <Tutors />
+      </Zoom>
     </main>
   );
 };
